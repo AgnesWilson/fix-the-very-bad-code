3ebf46bbda22519256078aba2c7bdd4b8c4e3a59
AgnesWilson <agnes.wilson97@hotmail.com>
2025-01-22 09:28:14 +0100

    💄 fixed smaller eslint issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+/* eslint-disable max-len */
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {files: ['**/*.{js,mjs,cjs,ts}']},
   {
     languageOptions: { globals: globals.browser },
     rules: {
